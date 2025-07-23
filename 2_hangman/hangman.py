17339669f84d41772c0b749c727c4b2294e61509
Abdullah <abdultoogle@gmail.com>
2025-07-23 14:23:32 +0500

    modified logic

--- 2_hangman/hangman.py
+++ 2_hangman/hangman.py
@@ -5,10 +5,10 @@ from wordsets import wordSets
 
 def fre(word): # returns frequency of each letter
     wordCounter = ""
-    wordCount = {} # dictionary to store the frequency of each unique letter
-
-    for i in range(0,len(word)):
-        char = word[i]
+    wordCount = {} # dictionary to store the frequency of each unique letter 
+        
+    for char in word:
+        indexList = [] # to store indices of each letter
         counter = 0
         if char in wordCounter: # to ensure each letter is only checked once
             continue
@@ -16,69 +16,75 @@ def fre(word): # returns frequency of each letter
         for j in range(0,len(word)):
             if char == word[j]:
                 counter +=1
+                indexList.append(j)
                 
-        wordCount.update({char:[counter,]})  
+        wordCount.update({char:[counter,indexList]})
         
     return(wordCount)
 
 
 def main():
-    category = random.choice(list(wordSets.keys()))
-    word = random.choice(wordSets[category])
+    category = random.choice(list(wordSets.keys())) # selects a random category
+    word = random.choice(wordSets[category]) # selects a random word from that category
     size = len(word)
     chances = size+3
-    guessed = ""
+    guessed = ["_" for i in range(0,size)] # list for storing guesses
     wordFre = fre(word)
     
     print(wordFre)
-    print(f"GUESS THE WORD\n\n HINT: The word is a {str(category).upper()}")
+    print(f"GUESS THE WORD\n\nHINT: The word is a {str(category).upper()}")
     print(f"You have {chances} chances to guess the correct letters. We will show one letter")
     print(word) # for testing
     
-    hintIndex = random.randint(0,size)
+    hintIndex = random.randint(0,size-1) # give the first letter as hint
     hint = word[hintIndex]
-    guessed += hint
+    guessed.insert(hintIndex,hint) # inserts that letter at that index
     wordFre[hint][0] -= 1
-    wordFre[hint].append([hintIndex,])
     
     print(wordFre)
     
     # if(wordFre[hint] == 0):
     #     del wordFre[hint]
     
-    for i in range(0, size):
-        if i == hintIndex:
-            print(hint, end= " ")
-        else:
-            print("_", end= " ")
+    for i in guessed: # prints the guessed leters
+        print(i,end=" ")
     print("\n") # to add a newline character
     
     while True:
-        guessedFre = fre(guessed)
         char = input("Enter a letter: ")
         
-        if not char.isalpha:
-            print("Enter an letter")
+        if not char.isalpha():
+            print("Only enter a letter")
             continue
 
         if len(char) > 1:
             print("Only enter one letter")
+            continue
         
         if char not in word:
             print("letter is not in word")
+            continue
             
         if char in guessed:
-            if wordFre[char] <= guessedFre[char]:
+            if wordFre[char][0] <= guessed.count(char):
                 print("letter already present")
                 continue
         
-        guessed += char
         
-        for i in range(0,size):
-            if char == word[i]:
-                print(char, end= " ")
-            else:
-                print("_", end= " ")
+        
+        # for i in range(0, size):
+        #     if char == word[i]:
+        #         index = wordFre[char][1]
+        
+        
+        # for i in range(0,size):
+        #     temp = guessed[i]
+        #     for j in range(0,size):
+        #         if temp == word[i]:
+                    
+        #             print(char, end= " ")
+        #         else:
+        #             print("_", end= " ")
         print("\n")
             
     
--- 2_hangman/wordsets.py
+++ 2_hangman/wordsets.py
@@ -116,4 +116,3 @@ wordSets = {
     "microwave", "broom", "mop", "bookshelf"
     ]
 }
-    
